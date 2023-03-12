9e8fdf27ef5baed29235d5ff783b005ce3ba43b0
lakardion <lakardion@protonmail.com>
2023-03-12 13:56:34 -0300

    chore: rm lib

--- lib/cjs/types/ts/v2/index.d.ts
+++
@@ -1,7 +0,0 @@
-export type { PaginationFilters } from "./api";
-export { createApi, createCustomServiceCall } from "./api";
-export { createCollectionManager } from "./collection-manager";
-export type { GetZodInferredTypeFromRaw, FromApiCall, ToApiCall } from "./utils";
-export { createApiUtils } from "./utils";
-export { parseResponse } from "./response";
-//# sourceMappingURL=index.d.ts.map
