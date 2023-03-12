d31c0ff3f5cac4709ca92ad63303cdccc19d9c9b
lakardion <lakardion@protonmail.com>
2023-03-12 17:15:42 -0300

    chore: minor change to script

--- index.ts
+++
@@ -1,8 +0,0 @@
-export { Pagination, PaginationDefaults, PaginationKwargs, IPagination } from "./src/pagination"
-
-export type { PaginationFilters } from "./src/api"
-export { createApi, createCustomServiceCall } from "./src/api"
-export { createCollectionManager } from "./src/collection-manager"
-export type { GetZodInferredTypeFromRaw, FromApiCall, ToApiCall } from "./src/utils"
-export { createApiUtils } from "./src/utils"
-export { parseResponse } from "./src/response"
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export { Pagination, PaginationDefaults, PaginationKwargs, IPagination } from "./pagination"
+
+export type { PaginationFilters } from "./api"
+export { createApi, createCustomServiceCall } from "./api"
+export { createCollectionManager } from "./collection-manager"
+export type { GetZodInferredTypeFromRaw, FromApiCall, ToApiCall } from "./utils"
+export { createApiUtils } from "./utils"
+export { parseResponse } from "./response"
