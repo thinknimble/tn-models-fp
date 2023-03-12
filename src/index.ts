b366e3aeb86c055cdd5ec50ad0a9d72a82a93568
lakardion <lakardion@protonmail.com>
2023-03-12 13:58:16 -0300

    chore: rm collection manager class

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,4 @@
-import CollectionManager, { ICollectionKwargs, ICollectionManager } from "./ts/collectionManager"
 import Pagination, { PaginationDefaults, PaginationKwargs, IPagination } from "./ts/pagination"
 export * from "./ts/v2"
 
-export {
-  CollectionManager,
-  ICollectionKwargs,
-  ICollectionManager,
-  Pagination,
-  PaginationDefaults,
-  PaginationKwargs,
-  IPagination,
-}
+export { Pagination, PaginationDefaults, PaginationKwargs, IPagination }
